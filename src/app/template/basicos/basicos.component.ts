e123ba3c55bb209213acc9377fdf2f9a651bc4b3
Jenn_07 <cuevasjennifer07@gmail.com>
2022-12-28 16:03:31 -0500

    Directiva Personalizada

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { CustomMinDirective } from '../directives/custom-min.directive';
 
 @Component({
   selector: 'app-basicos',
@@ -18,11 +19,22 @@ export class BasicosComponent implements OnInit {
   
   nombreValido(): boolean {
     return this.miFormulario?.controls['producto']?.invalid 
-            && this.miFormulario?.controls['producto'].touched
+            && this.miFormulario?.controls['producto'].touched;
   }
+
+  precioValido(): boolean {
+
+    return this.miFormulario?.controls['precio']?.touched  
+            && this.miFormulario?.controls['precio']?.value < 0;
+  }
+
   //guardar( miFormulario: NgForm ) {
   guardar( ) {
     console.log( this.miFormulario );
     
+  }
+
+  customDirective() {
+    return this.miFormulario?.controls['existencias']?.errors
   }
 }
