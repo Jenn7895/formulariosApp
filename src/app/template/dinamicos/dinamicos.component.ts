f5e37d45d78d95d9f0470234af82b35368fe7fb9
Jenn_07 <cuevasjennifer07@gmail.com>
2022-12-29 10:29:18 -0500

    Formularios Dinamicos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +17,36 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent  {
+  
+  //Propiedad enlazada al ngModel pero no al FORMULARIO 
+  nuevoJuego: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  persona : Persona ={
+    nombre: 'Jenn',
+    favoritos: [
+      { id:1, nombre: 'Metal Gear' },
+      { id:2, nombre: 'DeathStranding' },
+    ]
   }
 
+  guardar() {
+    console.log('formulario posteado');
+    
+  }
+
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push( {...nuevoFavorito} );
+    this.nuevoJuego = '';
+  }
+
+
+  eliminar( index: number ) {
+    this.persona.favoritos.splice(index, 1);
+  }
 }
