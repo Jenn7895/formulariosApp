77c53a5050f4fa085f7745264a8a239ea40eb5c6
Jenn_07 <cuevasjennifer07@gmail.com>
2023-01-03 16:33:51 -0500

    Formulario Reactivo Switches

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    genero: [ 'M', Validators.required ],
+    notificaciones: [ true, Validators.required ],
+    condiciones: [ false, Validators.requiredTrue ]
+  });
 
-  ngOnInit(): void {
+  persona = {
+    genero: 'F',
+    notificaciones: true,
   }
 
+
+  constructor( private fb: FormBuilder ) { }
+
+  ngOnInit() {
+    this.miFormulario.reset( {
+      ...this.persona,
+      condiciones: false
+    } );
+
+    // this.miFormulario.valueChanges.subscribe( form => {
+    //   delete form.condiciones;
+    //   this.persona = form;
+    // })
+    
+    // Por desestructuración
+    this.miFormulario.valueChanges.subscribe( ({ condiciones, ...restoDeArgumentos }) => {
+      //delete form.condiciones;
+      this.persona = restoDeArgumentos;
+    })
+  }
+
+  guardar() {
+
+    const formValue = {...this.miFormulario.value};
+    delete formValue.condiciones;
+
+    this.persona = formValue;
+    
+  }
 }
